7204b89dac3b006350e16c545126e7cb413843e2
Vitor Navarro <vitor.navarro87@gmail.com>
2011-09-14 00:58:36 -0300

    Inicio tela de fim de jogo

---
+++ Mimicmize/FinishGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  FinishGameViewController.h
+//  Mimicmize
+//
+//  Created by Vitor Navarro on 9/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FinishGameViewController : UIViewController
+
+@end
